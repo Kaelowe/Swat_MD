97d5acd0b458cbc7f07a3082c3117f52dc12293b
Kaelowe <110870140+Kaelowe@users.noreply.github.com>
2022-09-07 08:33:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ import { watchFile, unwatchFile } from 'fs'
 import moment from 'moment-timezone'
 
 global.owner = [
-  ['+62 816-4682-9895'],
-  ['+62 895-3170-4997'],
-  ['+62 856-2482-3115'],
-  ['+84 34 979 0091'],
-  ['+62 818-0545-0249'],
-  ['+62 821-9047-3135', 'Swat Official', true],
-  ['+62 895-3170-4997', 'Alfarabotz', true],
-  ['+62 813-4312-0634', 'SwaNezzz', true],
-  ['+84 34 979 0091', 'Johannes', true],
-  ['+62 818-0545-0249', 'Krisna', true],
+  ['+62 812-4290-8239'],
+  ['+62 812-4290-8239'],
+  ['+62 812-4290-8239'],
+  ['+62 812-4290-8239'],
+  ['+62 812-4290-8239'],
+  ['+62 812-4290-8239', 'Swat Official', true],
+  ['+62 812-4290-8239', 'Alfarabotz', true],
+  ['+62 812-4290-8239', 'SwaNezzz', true],
+  ['+62 812-4290-8239', 'Johannes', true],
+  ['+62 812-4290-8239', 'Krisna', true],
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -92,8 +92,8 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6285624823115'
-global.nomorown = '6282190473135'
+global.nomorbot = '6281242908239
+global.nomorown = '6281242908239'
 global.namebot = 'Alfara botz'
 global.nameown = 'SwattBot Official'
 
